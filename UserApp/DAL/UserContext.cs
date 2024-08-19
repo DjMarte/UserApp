d092f450e2a7a28ebe9dfd3fc266d4b748a8e041
DjMarte <alphat098@gmail.com>
2024-08-18 22:18:29 -0400

    Creé la carpeta DAL y el Contexto del Usuario.

---
+++ UserApp/DAL/UserContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shared.Models;
+
+namespace UserApp.DAL;
+
+public class UserContext : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+        optionsBuilder.UseInMemoryDatabase("UserDatabase");
+    }
+
+    public DbSet<User> Users { get; set; }
+}
