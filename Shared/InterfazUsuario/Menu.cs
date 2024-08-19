4ce72110f3dae494bbe19320b99624f4ab707e6e
DjMarte <alphat098@gmail.com>
2024-08-18 22:36:21 -0400

    Renombrando carpeta

---
+++ Shared/InterfazUsuario/Menu.cs
@@ -0,0 +1,10 @@
+﻿namespace Shared.InterfazUsuario;
+
+public class Menu
+{
+    public void Opciones() {
+        Console.WriteLine("Seleccione una opción: \n");
+        Console.WriteLine(" 1. Crear usuario\n 2. Listar usuario\n 3. Salir\n\n");
+        Console.WriteLine("Opcion: ");
+    }
+}
