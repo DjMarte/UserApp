4f5b47c95c913b94fa0e31a76b45be013262474d
DjMarte <alphat098@gmail.com>
2024-08-18 23:11:16 -0400

    Creé la clase "ListarUsuario"

---
+++ UserApp/OpcionesUsuario/ListarUsuarios.cs
@@ -0,0 +1,16 @@
+﻿using UserApp.DAL;
+
+namespace UserApp.OpcionesUsuario;
+
+public class ListarUsuarios
+{
+    public static void ListUsers() {
+        using (var context = new UserContext()) {
+            var users = context.Users.ToList();
+            Console.WriteLine("Usuarios en la base de datos: \n");
+            foreach (var user in users) {
+                Console.WriteLine($"ID: {user.Id}, Name: {user.Name}, Email: {user.Email}");
+            }
+        }
+    }
+}
