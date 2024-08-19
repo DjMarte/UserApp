e63b72124c094fe5a781b24ed174fa3e6a8178ff
DjMarte <alphat098@gmail.com>
2024-08-18 23:10:53 -0400

    Creé la carpeta "OpcionesUsuario" y la clase "CrearUsuario".

---
+++ UserApp/OpcionesUsuario/CrearUsuario.cs
@@ -0,0 +1,22 @@
+﻿using Shared.Models;
+using UserApp.DAL;
+
+namespace Shared.OpcionesUsuario;
+
+public class CrearUsuario
+{
+    public static void CreateUser() {
+        Console.Write("Ingrese el nombre del usuario: ");
+        var name = Console.ReadLine();
+        Console.Write("Ingrese el email del usuaio: ");
+        var email = Console.ReadLine();
+
+        using (var context = new UserContext()) { 
+            var user = new User { Name = name, Email = email };
+            context.Users.Add(user);
+            context.SaveChanges();
+        }
+
+        Console.WriteLine("Usuario creado correctamente!!!");
+    }
+}
